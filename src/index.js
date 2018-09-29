aa18c35a40b61f9852740558030e911a28531cb9
Bademiya <miyamirza.esc@gmail.com>
2018-09-29 23:53:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import {usersData} from 'usersData';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducers from 'reducers';
+import reducers from './reducers';
 
 const store = createStore(reducers,usersData);
 
