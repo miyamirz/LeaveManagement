c5b13cc74886fc558e45723b242f0b93f9cd1701
Bademiya <miyamirza.esc@gmail.com>
2018-09-30 12:47:41 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ()=> {
     return(
         <div className="Header">
-           <label style={{color:'#ffffff',fontSize:'30px',marginLeft:'10px',marginTop:'5px'}}>Leave Management Tool</label>
+           <label style={{color:'#ffffff',fontSize:'30px',marginLeft:'10px',marginTop:'20px'}}>Leave Management Tool</label>
            </div>
     )
 }
