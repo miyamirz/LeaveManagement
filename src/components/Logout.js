8c8a1214c2d5903ddc7a60c854358665f55aa58d
Bademiya <miyamirza.esc@gmail.com>
2018-09-30 16:23:52 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,6 +2,6 @@ import React from 'react';
 export default (props)=> {
   console.log(props)
       return(
-          <button className="btn btn-danger" onClick={props.handler}>Logout</button>
+          <button className="btn btn-danger" style={{position:'absolute',left:'90%'}} onClick={props.handler}>Logout</button>
       )
 }
